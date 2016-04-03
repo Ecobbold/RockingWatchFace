a61b82d964b3733e48a85e0accdab56291b0729f
Emilie Cobbold <ecobb042@uottawa.ca>
2016-04-03 14:41:47 -0400

    bounce works!

--- src/RockingWatchFace.c
+++ src/RockingWatchFace.c
@@ -6,17 +6,61 @@ static TextLayer *s_time_layer;
 static GFont s_time_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
-static GDrawCommandImage *s_command_image;
-static Layer *s_canvas_layer;
+////static GDrawCommandImage *s_command_image;
+////static Layer *s_canvas_layer;
 
 ////static BitmapLayer *s_text_ur_layer;
 ////static GBitmap *s_text_ur_bitmap;
+static GBitmap *s_bitmap = NULL;
+static BitmapLayer *s_bitmap_layer;
+static GBitmapSequence *s_sequence = NULL;
+
 
 //changing text (CT)
 static AppSync s_sync;
 static uint8_t s_sync_buffer[64];
 static BitmapLayer *s_icon_layer;
 static GBitmap *s_icon_bitmap = NULL;
+
+
+static void load_sequence();
+
+static void timer_handler(void *context) {
+  uint32_t next_delay;
+
+  // Advance to the next APNG frame
+  if(gbitmap_sequence_update_bitmap_next_frame(s_sequence, s_bitmap, &next_delay)) {
+    bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+    layer_mark_dirty(bitmap_layer_get_layer(s_bitmap_layer));
+
+    // Timer for that delay
+    app_timer_register(next_delay, timer_handler, NULL);
+  } else {
+    // Start again
+    load_sequence();
+  }
+}
+static void load_sequence() {
+  // Free old data
+  if(s_sequence) {
+    gbitmap_sequence_destroy(s_sequence);
+    s_sequence = NULL;
+  }
+  if(s_bitmap) {
+    gbitmap_destroy(s_bitmap);
+    s_bitmap = NULL;
+  }
+
+  // Create sequence
+  s_sequence = gbitmap_sequence_create_with_resource(RESOURCE_ID_ANIMATION_2);
+
+  // Create GBitmap
+  s_bitmap = gbitmap_create_blank(gbitmap_sequence_get_bitmap_size(s_sequence), GBitmapFormat8Bit);
+
+  // Begin animation
+  app_timer_register(1, timer_handler, NULL);
+}
+
 
 enum TextKey {
   TEXT_KEY = 0x0,        // TUPLE_INT
@@ -42,17 +86,15 @@ static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tup
   }
 }
 
-
-static void update_proc(Layer *layer, GContext *ctx) {
+/*static void update_proc(Layer *layer, GContext *ctx) {
   // Set the origin offset from the context for drawing the image
-  GPoint origin = GPoint(10, 20);
+ ///// GPoint origin = GPoint(10, 20);
   
 
   // Draw the GDrawCommandImage to the GContext
+ //// gdraw_command_image_draw(ctx, s_command_image, origin);
 
-  gdraw_command_image_draw(ctx, s_command_image, origin);
-
-}
+}*/
 
 static void main_window_load(Window *window) {
   
@@ -88,16 +130,22 @@ text_layer_set_font(s_time_layer, s_time_font);
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   
-  int rockx = 65;
-  int rocky = 102;
+  int rockx = 10;
+  int rocky = 50;
+  
+  //s_bitmap_layer = bitmap_layer_create(GRect(rockx,PBL_IF_ROUND_ELSE(rocky+5, rocky), bounds.size.w, bounds.size.h));
+   s_bitmap_layer = bitmap_layer_create(GRect(rockx,PBL_IF_ROUND_ELSE(rocky+5, rocky), bounds.size.w, bounds.size.h));
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+
+  load_sequence();
   // Create the canvas Layer
-  s_canvas_layer = layer_create(GRect(rockx,PBL_IF_ROUND_ELSE(rocky+5, rocky), bounds.size.w, bounds.size.h));
+ //// s_canvas_layer = layer_create(GRect(rockx,PBL_IF_ROUND_ELSE(rocky+5, rocky), bounds.size.w, bounds.size.h));
 
   // Set the LayerUpdateProc
-  layer_set_update_proc(s_canvas_layer, update_proc);
+ //// layer_set_update_proc(s_canvas_layer, update_proc);
 
   // Add to parent Window
-  layer_add_child(window_layer, s_canvas_layer);  
+ ///// layer_add_child(window_layer, s_canvas_layer);  
   
   //You rock text
    // Create GBitmap
@@ -135,8 +183,8 @@ gbitmap_destroy(s_background_bitmap);
 // Destroy BitmapLayer
 bitmap_layer_destroy(s_background_layer);
   
-   layer_destroy(s_canvas_layer);
-  gdraw_command_image_destroy(s_command_image);
+  //// layer_destroy(s_canvas_layer);
+ //// gdraw_command_image_destroy(s_command_image);
   
   //destroy you rock
   // Destroy GBitmap
@@ -150,7 +198,7 @@ bitmap_layer_destroy(s_background_layer);
     gbitmap_destroy(s_icon_bitmap);
   }
     bitmap_layer_destroy(s_icon_layer);
-
+  bitmap_layer_destroy(s_bitmap_layer);
 }
 
 static void update_time() {
@@ -173,22 +221,23 @@ update_time();
 static void init() {
 // Create main Window element and assign to pointer
   s_main_window = window_create();
-  window_set_background_color(s_main_window, GColorWhite);
-  // Set handlers to manage the elements inside the Window
+  window_set_background_color(s_main_window, GColorFromRGB(153, 217, 234));
+ #if defined(PBL_SDK_2)
+  window_set_fullscreen(s_main_window, true);
+#endif
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
-    .unload = main_window_unload
+    .unload = main_window_unload,
   });
-
-  // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+  
   // Make sure the time is displayed from the start
 update_time();
   
   // Register with TickTimerService
 tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
    // Create the object from resource file
-  s_command_image = gdraw_command_image_create_with_resource(RESOURCE_ID_ROCKFRIEND);
+ //// s_command_image = gdraw_command_image_create_with_resource(RESOURCE_ID_ROCKFRIEND);
 
 }
 
